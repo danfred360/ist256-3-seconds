6e6ee95244d64e44b2ee8ce9946d094566fa121f
Danny Frederick <dannyfrederick360@gmail.com>
2022-03-29 18:44:07 -0400

    almost done

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let end;
 let start;
 
-let minimum;
-let maximum;
+let minimum = 0;
+let maximum = 0;
+let sum = 0;
 let average;
 
 let index = 1;
@@ -72,45 +73,64 @@ function newAttempt() {
 
     switch(true) {
         case (difference == 0):
+            $("#button").removeClass("btn-danger");
+            $("#button").removeClass("btn-warning");
+            $("#button").removeClass("btn-primary");
+            $("#button").addClass("btn-success");
             console.log("green " + difference);
             break;
         case (0 < difference && difference < 200):
+            $("#button").removeClass("btn-danger");
+            $("#button").removeClass("btn-warning");
+            $("#button").addClass("btn-primary");
+            $("#button").removeClass("btn-success");
             console.log("blue " + difference);
             break;
         case (200 < difference && difference < 500):
+            $("#button").removeClass("btn-danger");
+            $("#button").addClass("btn-warning");
+            $("#button").removeClass("btn-primary");
+            $("#button").removeClass("btn-success");
             console.log("yellow " + difference);
             break;
         case (difference >= 500):
+            $("#button").addClass("btn-danger");
+            $("#button").removeClass("btn-warning");
+            $("#button").removeClass("btn-primary");
+            $("#button").removeClass("btn-success");
             console.log("red " + difference);
             break;
         default:
+            $("#button").addClass("btn-danger");
+            $("#button").removeClass("btn-warning");
+            $("#button").removeClass("btn-primary");
+            $("#button").removeClass("btn-success");
             console.log("red " + difference);
             break;
     }
 
     chart.series[0].addPoint([index, difference / 1000]);
 
-    document.getElementById('resultTableBody').append("<tr><th scope='row'>" + index + "</th><td>" + start + "</td><td>" + end + "</td><td>" + difference + "</td></tr>");
+    let startTime = new Date(start).getTime();
+    let endTime = new Date(end).getTime();
+
+    $("#resultTableBody").append("<tr><th scope='row'>" + index + "</th><td>" + startTime + "</td><td>" + endTime + "</td><td>" + difference + "</td></tr>");
 
     results.push(difference);
 
-    if (difference < minimum) {
+    if (difference < minimum || minimum == 0) {
       minimum = difference;
     }
 
-    if (difference > maximum) {
+    if (difference > maximum || maximum == 0) {
       maximum = difference;
     }
 
-    let sum;
-
-    for (var i = 0; i < results.length; i++) {
-      sum += i;
-    }
+    sum += difference;
 
     average = sum / index;
 
-    $("#totalInfo").text("Min: " + minimum + " - Max: " + maximum + " - Average: " + average);
+    $("#totalInfo").text("Min: " + minimum / 1000 + " - Max: " + maximum / 1000 + " - Average: " + average / 1000 + " - Total Attempts: " + index);
 
     index += 1;
   }
